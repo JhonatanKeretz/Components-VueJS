013cb3a2dc1f1506e454118eda59845c25caa5c3
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-03-31 16:32:51 -0300

    Add files via upload

---
+++ src/AppContadores.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contadores">
+  <app-contador v-for="c in 5" :key="c" />
+  </div>
+</template>
+
+<script>
+import AppContador from './AppContador.vue'
+  export default {
+  components: { 'app-contador': AppContador }
+
+ }
+
+</script>
+
+<style scoped>
+
+ div{
+      border: 1px solid red;
+ }
+
+</style>
